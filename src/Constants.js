8aff7dc03ed88a15b41ed8ba8debf3511f091632
Derek Brown <derek@brown.lt>
2022-09-09 09:05:34 -0500

    Update Hero URL

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
-export const HERO_IMAGE_URL = "https://ik.imagekit.io/wrxztkausbq0/careers/hero_1B1E2E4_W.png?ik-sdk-version=javascript-1.4.3&updatedAt=1662693468130";
+export const HERO_IMAGE_URL = "https://ik.imagekit.io/wrxztkausbq0/careers/hero_H6W53Iidm.png?ik-sdk-version=javascript-1.4.3&updatedAt=1662732282136";
 
 export const LOGO_IMAGE_URL = "https://ik.imagekit.io/wrxztkausbq0/careers/logo_WRJ0MDKSO.png?ik-sdk-version=javascript-1.4.3&updatedAt=1662693467134";
 
