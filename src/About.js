82d5c0d8e3f265ea6bfd21b9faddb3a76a73b32d
Derek Brown <derek@brown.lt>
2022-09-09 11:43:06 -0500

    [Various] 100 Million

--- src/About.js
+++ src/About.js
@@ -14,7 +14,7 @@ function About() {
             in building useful communication for a tokenized world.
             <div className="text-lg mt-4 text-neutral-gray3">The 1<sup>st</sup> million web3 users came in through the financial back doors of DeFi, speculation, and crypto's promise of self-banking.</div>
             <div className="text-lg mt-4 text-neutral-gray3">The next <em>hundred</em> million web3 users will enter through the non-financial front doors of tokenized community and data ownership...but we have to make it fun.</div>
-            <div className="text-lg mt-4 text-neutral-gray3">If you believe in delivering web3's promises of <span className="font-semibold text-neutral-gray2">ownership</span> and <span className="font-semibold text-neutral-gray2">freedom</span> to the next 100 million users, we’d love to talk to you!</div>
+            <div className="text-lg mt-4 text-neutral-gray3">If you believe in delivering web3's promises of <span className="font-semibold text-neutral-gray2">ownership</span> and <span className="font-semibold text-neutral-gray2">freedom</span> to the next hundred million users, we’d love to chat!</div>
           </div>
           <div className="flex flex-row items-center w-full sm:w-auto">
             <a className="w-1/2 sm:w-40 bunches-button with-icon mr-4" rel="noreferrer" target="_blank" href="https://www.notion.so/bunches/Who-We-Are-3658aaee30f5474e9c49c9df0d2f2e70"><FaGrinHearts className="mr-2 sm:mr-4"/>About Us</a>
