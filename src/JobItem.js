72f86fd4223553691b5db9ca59dbd2349b01458a
Derek Brown <derek@brown.lt>
2022-09-09 12:02:23 -0500

    Link Job Items

--- src/JobItem.js
+++ src/JobItem.js
@@ -1,7 +1,7 @@
 function JobItem({job}) {
   return (
     <>
-    <div className="flex flex-col sm:flex-row items-center sm:hover:bg-neutral-gray7 rounded-xl py-4 -ml-6 pl-6 pr-6 -mr-6 cursor-pointer my-4">
+    <a href={job.additionalPlain} rel="noreferrer" target="_blank" className="block flex flex-col sm:flex-row items-center sm:hover:bg-neutral-gray7 rounded-xl py-4 -ml-6 pl-6 pr-6 -mr-6 cursor-pointer my-4">
       <div className="flex-0">
         <h2 className="font-bold text-xl sm:text-2xl text-neutral-gray2 flex-1">{job.text}</h2>
         <span className="hidden sm:block -mt-1 mr-1 text-sm text-neutral-gray3 rounded-md">{job.categories.team}</span>
@@ -10,7 +10,7 @@ function JobItem({job}) {
         <a className="w-1/2 sm:w-36 mr-4 bunches-button" rel="noreferrer" target="_blank" href={job.additionalPlain}>About</a>
         <a className="w-1/2 sm:w-36 mr-2 bunches-button" rel="noreferrer" target="_blank" href={job.hostedUrl}>Apply</a>
       </div>
-    </div>
+    </a>
     <div className="block flex w-full bg-neutral-gray6 h-0.5"></div>
     </>
   );
