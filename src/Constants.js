4bd583b9989ca18d9591fdb92f0bc281bc0f8487
Derek Brown <derek@brown.lt>
2022-09-11 11:53:17 -0500

    [Header] Logo

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,7 @@
+import app_logo from "./assets/images/app_logo.png";
 import hero from "./assets/images/hero.png";
+
+export const APP_LOGO_IMAGE_URL = app_logo;
 export const HERO_IMAGE_URL = hero;
-
 export const LOGO_IMAGE_URL = "https://ik.imagekit.io/wrxztkausbq0/careers/logo_WRJ0MDKSO.png?ik-sdk-version=javascript-1.4.3&updatedAt=1662693467134";
-
 export const PHONE_IMAGE_URL = "https://ik.imagekit.io/wrxztkausbq0/careers/profile_93Bb87B-D.png?ik-sdk-version=javascript-1.4.3&updatedAt=1662693466872";
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,9 @@
-import { LOGO_IMAGE_URL } from "./Constants";
+import { APP_LOGO_IMAGE_URL } from "./Constants";
 
 function Header({job}) {
   return (
     <div className="flex flex-col w-full justify-center items-center mt-8">
-      <img src={LOGO_IMAGE_URL} alt="Bunches Logo" className="h-24 w-24"/>
+      <img src={APP_LOGO_IMAGE_URL} alt="Bunches Logo" className="h-24 w-24"/>
       <h2 className="text-6xl font-bold mb-8">Careers</h2>
     </div>
   );
