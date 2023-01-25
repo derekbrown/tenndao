ff1f84d1367827257e6aacafd9e44e4b552aa2d7
Derek Brown <derek@brown.lt>
2023-01-25 10:04:38 -0600

    Update Description

--- src/About.js
+++ src/About.js
@@ -11,11 +11,11 @@ function About() {
         <div className="w-full sm:w-2/3">
           <div className="text-3xl sm:text-4xl text-neutral-gray2 mb-8">
             <span className="font-bold text-neutral-gray1">Join us </span>
-            in building useful communication for a tokenized world.
-            <div className="text-lg mt-4 text-neutral-gray3">Bunches is a mobile-first <strong className="font-semibold">web3 messenger</strong> with an integrated social wallet, designed specifically for creators and their communities.</div>
+            in increasing freedom and ownership for the world's creators!
+            <div className="text-lg mt-4 text-neutral-gray3">Bunches is building a super app that weaves together chat, content, commerce, and community: built for creators, designed for consumers, and enabled by web3.</div>
             <div className="text-lg mt-4 text-neutral-gray3">The 1<sup>st</sup> million web3 users came in through the financial back doors of DeFi, speculation, and crypto's promise of self-banking.</div>
             <div className="text-lg mt-4 text-neutral-gray3">The next <em>hundred</em> million web3 users will enter through the non-financial front doors of tokenized community and data ownership...but we have to make it fun.</div>
-            <div className="text-lg mt-4 text-neutral-gray3">If you believe in delivering web3's promises of <span className="font-semibold text-neutral-gray2">ownership</span> and <span className="font-semibold text-neutral-gray2">freedom</span> to the next hundred million users, we’d love to chat!</div>
+            <div className="text-lg mt-4 text-neutral-gray3">If you believe in delivering web3's promises of <span className="font-semibold text-neutral-gray2">ownership</span> and <span className="font-semibold text-neutral-gray2">freedom</span> to the world's creators, we’d love to chat!</div>
           </div>
           <div className="flex flex-row items-center w-full sm:w-auto">
             <a className="w-1/2 sm:w-40 bunches-button with-icon mr-4" rel="noreferrer" target="_blank" href="https://www.notion.so/bunches/Who-We-Are-3658aaee30f5474e9c49c9df0d2f2e70"><FaGrinHearts className="mr-2 sm:mr-4"/>About Us</a>
